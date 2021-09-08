7c51cd206000ed7fdd5bbe21f2df53212c446212
Zeke Krug <krug0102@morris.umn.edu>
2021-09-08 18:44:21 -0500

    Re-created wrap_contents.sh

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-#Finds all the necessary files and writes them to a new file with the name given.
-#Where "${2}_header" is the correct header html file, "$1" is the text file to be
-#wrapped, and "${2}_footer" is the correct footer html file.
 cat "${2}_header".html "$1" "${2}_footer".html > "$3"
