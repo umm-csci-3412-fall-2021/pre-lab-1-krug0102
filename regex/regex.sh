7ffa7b8770203df07c97f369d36f88f17edc9901
Zeke Krug <krug0102@morris.umn.edu>
2021-09-08 12:50:38 -0500

    Updated regex.sh

--- regex/regex.sh
+++ regex/regex.sh
@@ -7,4 +7,5 @@ fname="${iname}output.txt"
 #creates a .txt file with the correct name.
 >"$fname"
 
+awk 'match($0, /([a-zA-Z]+), ([a-zA-Z]+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" > "$fname" }' < "$1"
 
