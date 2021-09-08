15db453a9eb76b567ed2382d431061fa88dac68c
Zeke Krug <krug0102@morris.umn.edu>
2021-09-08 15:24:06 -0500

    Ran tests Ran bats regex_tests.sh and bats wrap_tests.sh

--- regex/regex.sh
+++ regex/regex.sh
@@ -5,7 +5,7 @@ sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_outpu
 
 #Only looks at lines whose first sentence starts with "* I am..." and whose
 # second line starts with "My favorite sandwich is..."
-sed -E 's/\* I am ([a-zA-Z]+)\. My favorite sandwich is ([a-z]+)\./1. \1\n2. \2\n/;t;d'<r1_input.txt > r1_output.txt
+sed -E 's/\* I am ([a-zA-Z]+)\. My favorite sandwich is ([a-z]+)\./1. \1\n2. \2\n/;t;d' <r1_input.txt > r1_output.txt
 
 #Takes the following string (including .) following "* sandwich with ..." and the
 # rest of the line following the period.
